72e44a27da3fa78c96fb1b61e8a92d9e5d23822f
Michael Jackson <mjijackson@gmail.com>
2014-12-30 16:54:43 -0800

    Use named functions, remove uuid

--- modules/grand.js
+++ modules/grand.js
@@ -1,41 +1,47 @@
-var fs = require('fs');
+var FAMILY_NAMES = require('./data/names').family;
+var FEMALE_NAMES = require('./data/names').female;
+var MALE_NAMES = require('./data/names').male;
+var LOCALES = require('./data/locales').locales;
+var TIMEZONES = require('./data/timezones').timezones;
+var NUMBERS = '1234567890'.split('');
+var LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var LETTERS = LOWER_LETTERS.concat(UPPER_LETTERS);
+var WORD_CHARS = [ '_' ].concat(NUMBERS).concat(LETTERS);
 
-// Requires the wordlist package on Linux.
-var possibleWordsFiles = [ '/usr/share/dict/words', '/usr/dict/words' ];
+var _words;
+function randomWords() {
+  if (!_words) {
+    if (typeof window === 'undefined') {
+      var fs = require('fs');
 
-var _wordsFile;
-exports.__defineGetter__('wordsFile', function () {
-  if (!_wordsFile) {
-    var existingWordsFiles = possibleWordsFiles.filter(function (file) {
-      return fs.existsSync(file);
-    });
+      // Requires the wordlist package on Linux.
+      var possibleWordsFiles = [ '/usr/share/dict/words', '/usr/dict/words' ];
+      var existingWordsFiles = possibleWordsFiles.filter(function (file) {
+        return fs.existsSync(file);
+      });
 
-    if (existingWordsFiles.length) {
-      _wordsFile = existingWordsFiles[0];
+      if (!existingWordsFiles.length)
+        throw new Error('grand: Words file not found in ' + possibleWordsFiles.join(', '));
+
+      var wordsFile = existingWordsFiles[0];
+      var allLetters = /^[a-zA-Z]+$/;
+
+      _words = fs.readFileSync(wordsFile, 'utf8').split('\n').filter(function (word) {
+        return allLetters.test(word);
+      });
     } else {
-      throw new Error('Words file not found in ' + possibleWordsFiles.join(', '));
+      throw new Error('grand: Needs some random words');
     }
   }
 
-  return _wordsFile;
-});
-
-var _words;
-exports.__defineGetter__('words', function () {
-  if (!_words) {
-    var allLetters = /^[a-zA-Z]+$/;
-    _words = fs.readFileSync(exports.wordsFile, 'utf8').split('\n').filter(function (word) {
-      return allLetters.test(word);
-    });
-  }
-
   return _words;
-});
+}
 
 var _wordsByLength;
-exports.__defineGetter__('wordsByLength', function () {
+function randomWordsByLength() {
   if (!_wordsByLength) {
-    _wordsByLength = exports.words.reduce(function (memo, word) {
+    _wordsByLength = randomWords().reduce(function (memo, word) {
       var length = word.length;
 
       if (memo[length]) {
@@ -49,84 +55,74 @@ exports.__defineGetter__('wordsByLength', function () {
   }
 
   return _wordsByLength;
-});
+}
 
-exports.number = function (exclusiveMax) {
+function randomNumber(exclusiveMax) {
   return Math.random() * (exclusiveMax || 1);
-};
+}
 
-exports.integer = function (exclusiveMax) {
-  return exports.number(exclusiveMax) | 0;
-};
+function randomInteger(exclusiveMax) {
+  return randomNumber(exclusiveMax) | 0;
+}
 
-exports.pick = function (array) {
-  return array[exports.integer(array.length)];
-};
+function randomArrayItem(array) {
+  return array[randomInteger(array.length)];
+}
 
-var NUMBERS = '1234567890'.split('');
-var LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-var LETTERS = LOWER_LETTERS.concat(UPPER_LETTERS);
-var WORD_CHARS = [ '_' ].concat(NUMBERS).concat(LETTERS);
+function randomLetter() {
+  return randomArrayItem(LETTERS);
+}
 
-exports.letter = function () {
-  return exports.pick(LETTERS);
-};
+function randomWordChar() {
+  return randomArrayItem(WORD_CHARS);
+}
 
-exports.wordChar = function () {
-  return exports.pick(WORD_CHARS);
-};
-
-exports.word = function (length) {
-  length = length || Math.max(2, exports.integer(12));
+function randomWord(length) {
+  length = length || Math.max(2, randomInteger(12));
 
   var choices;
   do {
-    choices = exports.wordsByLength[length];
+    choices = randomWordsByLength[length];
     length -= 1;
   } while (!choices); // Make sure we have a valid length.
 
-  return exports.pick(choices);
-};
+  return randomArrayItem(choices);
+}
 
-exports.sentence = function (maxWords) {
-  maxWords = Math.max(3, maxWords || exports.integer(12));
+function randomSentence(maxWords) {
+  maxWords = Math.max(3, maxWords || randomInteger(12));
 
   var words = [];
-  for (var i = 0; i < maxWords; i++) {
-    words.push(exports.word(exports.integer(12)));
-  }
+  for (var i = 0; i < maxWords; i++)
+    words.push(randomWord(randomInteger(12)));
 
   return words.join(' ');
-};
+}
 
-exports.gender = function () {
-  return exports.number() > 0.5 ? 'male' : 'female';
-};
+function randomGender() {
+  return randomNumber() > 0.5 ? 'male' : 'female';
+}
 
-var names = require('./data/names');
+function randomGivenName(gender) {
+  gender = gender || randomGender();
+  return randomArrayItem(gender === 'male' ? MALE_NAMES : FEMALE_NAMES);
+}
 
-exports.givenName = function (gender) {
-  gender = gender || exports.gender();
-  return exports.pick(gender === 'male' ? names.male : names.female);
-};
+function randomFamilyName() {
+  return randomArrayItem(FAMILY_NAMES);
+}
 
-exports.familyName = function () {
-  return exports.pick(names.family);
-};
+function randomName(gender) {
+  return [ randomGivenName(gender), randomFamilyName() ].join(' ');
+}
 
-exports.name = function (gender) {
-  return [ exports.givenName(gender), exports.familyName() ].join(' ');
-};
+function randomEmailAddress(tld) {
+  tld = tld || '.com';
+  return (randomGivenName() + '@' + randomWord() + '.com').toLowerCase();
+}
 
-exports.emailAddress = function () {
-  return (exports.givenName() + '@' + exports.word() + '.com').toLowerCase();
-};
-
-var locales = require('./data/locales').locales;
-
-exports.locale = function (region) {
-  var choices = locales.slice(0);
+function randomLocale(region) {
+  var choices = LOCALES.slice(0);
 
   if (region) {
     choices = choices.filter(function (locale) {
@@ -134,13 +130,11 @@ exports.locale = function (region) {
     });
   }
 
-  return exports.pick(choices);
-};
+  return randomArrayItem(choices);
+}
 
-var timezones = require('./data/timezones').timezones;
-
-exports.timezone = function (continent) {
-  var choices = timezones.slice(0);
+function randomTimezone(continent) {
+  var choices = TIMEZONES.slice(0);
 
   if (continent) {
     choices = choices.filter(function (zone) {
@@ -148,9 +142,27 @@ exports.timezone = function (continent) {
     });
   }
 
-  return exports.pick(choices);
+  return randomArrayItem(choices);
+}
+
+module.exports = {
+  words: randomWords,
+  wordsByLength: randomWordsByLength,
+  number: randomNumber,
+  num: randomNumber,
+  integer: randomInteger,
+  int: randomInteger,
+  arrayItem: randomArrayItem,
+  pick: randomArrayItem,
+  letter: randomLetter,
+  wordChar: randomWordChar,
+  word: randomWord,
+  sentence: randomSentence,
+  gender: randomGender,
+  givenName: randomGivenName,
+  familyName: randomFamilyName,
+  name: randomName,
+  emailAddress: randomEmailAddress,
+  locale: randomLocale,
+  timezone: randomTimezone
 };
-
-var uuid = require('node-uuid');
-
-exports.uuid = uuid.v4;
